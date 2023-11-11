39fc3c962f80324b051dfa792cb43abb729af02c
kdu2030 <kdu2030@gmail.com>
2023-11-11 17:10:30 -0500

    Create Recipe Provider Data Gen

---
+++ src/main/java/kevin/tutorialmod/datagen/ModRecipeProvider.java
@@ -0,0 +1,45 @@
+package kevin.tutorialmod.datagen;
+import kevin.tutorialmod.block.ModBlocks;
+import kevin.tutorialmod.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Consumer;
+import java.util.List;
+
+public class ModRecipeProvider extends FabricRecipeProvider {
+    private static final List<ItemConvertible> RUBY_SMELTABLES = List.of(ModItems.RAW_RUBY, ModBlocks.RUBY_ORE,
+            ModBlocks.DEEPSLATE_RUBY_ORE, ModBlocks.NETHER_RUBY_ORE, ModBlocks.END_STONE_RUBY_ORE);
+
+    public ModRecipeProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generate(Consumer<RecipeJsonProvider> exporter) {
+        offerSmelting(exporter, RUBY_SMELTABLES, RecipeCategory.MISC, ModItems.RUBY, 0.7f, 200, "ruby");
+        offerBlasting(exporter, RUBY_SMELTABLES, RecipeCategory.MISC, ModItems.RUBY, 0.7f, 100, "ruby");
+
+        // This is how you create recipes where the entire crafting grid is filled up with one item and it produces a block
+        // This will also create a recipe where if you put the block into the crafting table you will get 9 of a corresponding item
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, ModItems.RUBY, RecipeCategory.DECORATIONS, ModBlocks.RUBY_BLOCK);
+
+        // Criterion - These lines will make the recipe appear in the recipe book when the user has either stone or ruby in their inventory
+        // new Identifier(...) - This will make the recipe have the same name as ModItems.RAW_RUBY
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.RAW_RUBY, 1)
+                .pattern("SSS")
+                .pattern("SRS")
+                .pattern("SSS")
+                .input('S', Items.STONE)
+                .input('R', ModItems.RUBY)
+                .criterion(hasItem(Items.STONE), conditionsFromItem(Items.STONE))
+                .criterion(hasItem(ModItems.RUBY), conditionsFromItem(ModItems.RUBY))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.RAW_RUBY)));
+    }
+}
